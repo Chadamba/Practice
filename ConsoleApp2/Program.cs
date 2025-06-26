7d9ebffe8203f10d6a9ac7a8a934ad6c24739697
Chaam <Chaam@DESKTOP-62C01KF>
2025-06-26 11:11:00 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+/*
+Console.Write("Введите имя: ");
+string? name = Console.ReadLine();
+Console.Write("Введите возраст: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите рост: ");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Имя:{name} Возраст:{age} Рост:{height}m");
+Console.WriteLine();
+*/
+
+/*
+Console.WriteLine("Простой калькулятор расхода топлива");
+Console.Write("Введите расстояние (км): ");
+double dist = Convert.ToDouble(Console.ReadLine());
+Console.Write("Потраченое топливо(л): ");
+double vol = Convert.ToDouble(Console.ReadLine());
+double cons = (100 * vol) / dist;
+string output = string.Format("Автомобиль прошел {0:F2}км потратил {1:F2} литров топлива Средний расход - {2:F3} л/100км", dist, vol, cons);
+Console.WriteLine(output);
+*/
+/*
+Console.WriteLine("Введите делимое: ");
+double divident = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите делитель: ");
+double divisor = Convert.ToDouble(Console.ReadLine());
+double quotient = divident / divisor;
+double remainder = divident % divisor;
+Console.WriteLine($"Частное = {quotient} Остаток = {remainder}");
+Console.ReadLine();
+*/
+
+
+
